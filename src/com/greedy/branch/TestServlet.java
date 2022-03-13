9db789f7a5462a0a7e167de59f99ece6dce19be9
Cota <taegyoung98@gmail.com>
2022-03-14 03:39:52 +0900

    first commit

---
+++ src/com/greedy/branch/TestServlet.java
@@ -0,0 +1,22 @@
+package com.greedy.branch;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("test", "git branch test입니다.");
+		request.getRequestDispatcher("/WEB-INF/views/branch/result.jsp").forward(request,response);
+	}
+
+}
